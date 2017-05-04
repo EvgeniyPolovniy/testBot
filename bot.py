29851fe9d93e3d0a292aff4f525abc627d3455d0
Evgeniy Polovniy <epolovniy@webinerds.com>
2017-05-04 14:46:56 +0300

    Add kitten

--- bot.py
+++ bot.py
@@ -9,11 +9,14 @@ def echo_msg(message):
 
 @bot.message_handler(commands=['info'])
 def echo_msg(message):
+    photo = open('./img/cat.jpg', 'rb')
+    bot.send_photo(message.chat.id, photo)
     bot.send_message(message.chat.id, message)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_msg(message):
     bot.send_message(message.chat.id, message.text)
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
